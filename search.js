293a0d016588538699ba3ff490b940dc58d7f7b2
sandraNotTaken <155741111+sandraNotTaken@users.noreply.github.com>
2026-01-30 23:57:09 +0100

    search: Add search button and results container

--- search.js
+++ search.js
@@ -1,11 +1,29 @@
 const apiKey = "a84f5801";
 const query = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("click", function() {
+    searchMovie(query.value);
+});
+
 function searchMovie(query) {
     fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${encodeURIComponent(query)}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        // Handle the search results here
+        console.log(data.Search);
+        const resultsContainer = document.getElementById("results-container");
+        resultsContainer.innerHTML = ""; // Clear previous results
+
+        data.Search.forEach(movie => {
+            const movieElement = document.createElement("div");
+            movieElement.innerHTML = `
+                <img src="${movie.Poster}" alt="${movie.Title}">
+                <div class="movie-info">
+                    <h3>${movie.Title}</h3>
+                </div>
+            `;
+            resultsContainer.appendChild(movieElement);
+        })
     })
     .catch(error => {
         console.error("Error fetching data:", error);
